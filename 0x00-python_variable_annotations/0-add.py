69ef4308838d5ff4b36a59fcc4bc0a549fa0b5b4
adesanyaolu <sanya4reel@gmail.com>
2023-02-02 15:09:50 +0100

    Task 1 and 2

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    Return the sum of two floating-point numbers.
+    
+    Arguments:
+    a -- The first floating-point number
+    b -- The second floating-point number
+
+    Returns:
+    The sum of the two input numbers as a floating-point value.
+    """
+    return a + b
