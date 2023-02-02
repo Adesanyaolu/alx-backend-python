8e576f4723d58ba40fc6a2224a8dbe33b71996f4
adesanyaolu <sanya4reel@gmail.com>
2023-02-02 15:49:34 +0100

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def floor(n: float) -> int :
+    """
+    Return the floor of a floating-point number.
+    
+    Arguments:
+    n -- A floating-point number
+
+    Returns:
+    The largest integer less than or equal to n.
+    """
+    return int(math.floor(n))
