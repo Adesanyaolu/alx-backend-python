91b7aa3d84595f126ea67a4f52e9fe8a8be33e94
adesanyaolu <sanya4reel@gmail.com>
2023-02-02 20:41:20 +0100

    Task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,7 +5,6 @@ and a float b as arguments and returns their sum as a float."""
 
 def add(a: float, b: float) -> float:
     """
-
     Return the sum of two floating-point numbers.
     Arguments:
     a -- The first floating-point number
